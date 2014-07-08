b4cc1dd9cd9d10e20e5100f61f23525ac65a977f
artingei <artingei@gmail.com>
2014-07-08 17:06:13 +0200

    NEW

--- js/script.js
+++ js/script.js
@@ -21,10 +21,10 @@ $(function(){
         var stickyHeaderTop = $('header').offset().top;
         if ($(window).width() > 960) {
             $(window).scroll(function(){
-                    if( $(window).scrollTop() > 100 ) {
-                            $('header').css({position: 'fixed', height: '36px', transition: 'all 0.2s ease-in-out', transitionDelay: '0.25s',});
-                            $('header .fit').css({top: '0px', height: '36px', transition: 'all 0.2s ease-in-out', transitionDelay: '0.25s',});
-                            $('.logo').css({opacity: '0', transition: 'all 0.25s ease-in-out',});
+                    if( $(window).scrollTop() > 40 ) {
+                            $('header').css({position: 'fixed', height: '36px', transition: 'all 0.2s ease-in-out', transitionDelay: '0.2s',});
+                            $('header .fit').css({top: '0px', height: '36px', transition: 'all 0.2s ease-in-out', transitionDelay: '0.2s',});
+                            $('.logo').css({opacity: '0', transition: 'all 0.2s ease-in-out',});
                             $('header nav ul li').css({borderRadius: '0px',});
                     } else {
                             $('header').css({position: 'fixed', height: '80px', transition: 'all 0.3s ease-in-out'});
@@ -37,3 +37,53 @@ $(function(){
         } else {
         }
   });
+
+$(document).ready(function(){
+    $('a[href^="#"]').on('click',function (e) {
+        e.preventDefault();
+
+        var target = this.hash,
+        $target = $(target);
+
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top
+        }, 900, 'swing', function () {
+            window.location.hash = target;
+        });
+    });
+});
+
+$(document).ready(function(){
+var aChildren = $("nav li").children(); // find the a children of the list items
+    var aArray = []; // create the empty aArray
+    for (var i=0; i < aChildren.length; i++) {    
+        var aChild = aChildren[i];
+        var ahref = $(aChild).attr('href');
+        aArray.push(ahref);
+    } // this for loop fills the aArray with attribute href values
+
+    $(window).scroll(function(){
+        var windowPos = $(window).scrollTop(); // get the offset of the window from the top of page
+        var windowHeight = $(window).height(); // get the height of the window
+        var docHeight = $(document).height();
+
+        for (var i=0; i < aArray.length; i++) {
+            var theID = aArray[i];
+            var divPos = $(theID).offset().top; // get the offset of the div from the top of page
+            var divHeight = $(theID).height(); // get the height of the div in question
+            if (windowPos >= divPos && windowPos < (divPos + divHeight)) {
+                $("a[href='" + theID + "']").addClass("active");
+            } else {
+                $("a[href='" + theID + "']").removeClass("active");
+            }
+        }
+
+        if(windowPos + windowHeight == docHeight) {
+            if (!$("nav li:last-child a").hasClass("nav-active")) {
+                var navActiveCurrent = $(".nav-active").attr("href");
+                $("a[href='" + navActiveCurrent + "']").removeClass("nav-active");
+                $("nav li:last-child a").addClass("nav-active");
+            }
+        }
+    });
+});
