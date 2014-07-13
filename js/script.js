490455431705483e9771473c289c3195a4a443bc
artingei <artingei@gmail.com>
2014-07-13 13:43:03 +0200

    json

--- js/script.js
+++ js/script.js
@@ -87,3 +87,33 @@ var aChildren = $("nav li").children(); // find the a children of the list items
         }
     });
 });
+
+$(window).load(function(){
+        $('#search').keyup(function(){
+            var searchField = $('#search').val();
+            var regex = new RegExp(searchField, "i");
+            var output = '<div class="row">';
+            var count = 1;
+            $.getJSON('json/db.json', function(data) {
+                console.log(data);
+              $.each(data, function(key, val){
+                console.log(val);
+                // if ((val.name.search(regex) != -1) || (val.location.search(regex) != -1)) {
+                //   output += '<div class="col-md-6 well">';
+                //   output += '<div class="col-md-3"><img class="img-responsive" src="'+val.avatar+'" alt="'+ val.name +'" /></div>';
+                //   output += '<div class="col-md-7">';
+                //   output += '<h5>' + val.name + '</h5>';
+                //   output += '<p>' + val.location + '</p>'
+                //   output += '</div>';
+                //   output += '</div>';
+                //   if(count%2 == 0){
+                //     output += '</div><div class="row">'
+                //   }
+                //   count++;
+                // }
+              });
+              output += '</div>';
+              $('#results').html(output);
+            }); 
+        });
+      });
