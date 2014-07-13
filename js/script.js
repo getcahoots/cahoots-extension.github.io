0b48795690667bced7e04df3dbf0091646fc6700
artingei <artingei@gmail.com>
2014-07-13 13:44:58 +0200

    again

--- js/script.js
+++ js/script.js
@@ -95,22 +95,20 @@ $(window).load(function(){
             var output = '<div class="row">';
             var count = 1;
             $.getJSON('json/db.json', function(data) {
-                console.log(data);
               $.each(data, function(key, val){
-                console.log(val);
-                // if ((val.name.search(regex) != -1) || (val.location.search(regex) != -1)) {
-                //   output += '<div class="col-md-6 well">';
-                //   output += '<div class="col-md-3"><img class="img-responsive" src="'+val.avatar+'" alt="'+ val.name +'" /></div>';
-                //   output += '<div class="col-md-7">';
-                //   output += '<h5>' + val.name + '</h5>';
-                //   output += '<p>' + val.location + '</p>'
-                //   output += '</div>';
-                //   output += '</div>';
-                //   if(count%2 == 0){
-                //     output += '</div><div class="row">'
-                //   }
-                //   count++;
-                // }
+                if ((val.name.search(regex) != -1) || (val.location.search(regex) != -1)) {
+                  output += '<div class="col-md-6 well">';
+                  output += '<div class="col-md-3"><img class="img-responsive" src="'+val.avatar+'" alt="'+ val.name +'" /></div>';
+                  output += '<div class="col-md-7">';
+                  output += '<h5>' + val.name + '</h5>';
+                  output += '<p>' + val.location + '</p>'
+                  output += '</div>';
+                  output += '</div>';
+                  if(count%2 == 0){
+                    output += '</div><div class="row">'
+                  }
+                  count++;
+                }
               });
               output += '</div>';
               $('#results').html(output);
