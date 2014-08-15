f30d84b4ff050ffe4b3e4d053fb4e5815fcc16da
artingei <jonasbergmeier@jonas.local>
2014-08-15 18:18:56 +0200

    …

--- suchen/js/main.js
+++ suchen/js/main.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
 			$('#del').css({ display: 'none',});
 		} else {
 			$('header').css({ backgroundPosition: 'left center', margin: '0 auto 0 auto', transition: 'all 0.3s linear', WebkitTransition: 'all 0.3s linear', WebkitTransitionDelay: '0.05s', transitionDelay: '0.05s',});
-			$('.results').css({ opacity: '1', WebkitOpacity: '1', margin: '25px 0 80px 0', transition: 'all 0.4s cubic-bezier(0,.99,.66,.99)', WebkitTransition: 'all 0.4s cubic-bezier(0,.99,.66,.99)', WebkitTransitionDelay: '1s', transitionDelay: '1s',});
+			$('.results').css({ opacity: '1', WebkitOpacity: '1', margin: '25px 0 80px 0', transition: 'all 0.4s cubic-bezier(0,.99,.66,.99)', WebkitTransform: 'scale(1)', MozTransform: 'scale(1)', MsTransform: 'scale(1)', OTransform: 'scale(1)', transform: 'scale(1)', WebkitTransition: 'all 0.4s cubic-bezier(0,.99,.66,.99)', WebkitTransitionDelay: '1s', transitionDelay: '1s',});
 			$('#del').css({ display: 'none',});
 		}
 	});
